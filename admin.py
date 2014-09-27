b35102764c0b1358f2f39c79e6537385c075dc2a
Francisco Modesto <fran.bcn@gmail.com>
2014-09-27 16:09:22 +0100

    Add credentials

---
+++ admin.py
@@ -0,0 +1,9 @@
+from expenses import db, User, hash_password
+
+
+def create_db():
+    db.create_all()
+
+
+def add_user(email, password):
+    db.session.add(User(email=email, password=hash_password(password)))
--- expenses.py
+++ expenses.py
@@ -1,15 +1,18 @@
-from flask.helpers import send_from_directory
-import os
 import uuid
 import urllib
 from datetime import date
 from decimal import Decimal
-from flask import Flask, render_template, url_for, redirect
+
+from flask.ext.login import login_required, UserMixin, current_user, LoginManager, login_user, logout_user
+from flask.helpers import send_from_directory, flash
+import os
+from flask import Flask, render_template, url_for, redirect, request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
-from wtforms import DecimalField, SelectField, DateField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import DecimalField, SelectField, DateField, SubmitField, HiddenField, StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email
 from wtforms.widgets.html5 import NumberInput, DateInput
 
 
@@ -25,15 +28,17 @@ app.config['EXPENSES_CONCEPT'] = [('MEALS', 'Meals'), ('TRANSPORT', 'Transport')
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message = None
 
 
-class Expense(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Text, nullable=False)
-    currency = db.Column(db.Enum('GBR', 'EUR'), nullable=False)
-    concept = db.Column(db.Enum('MEALS', 'ACCOMODATION', 'TRANSPORT', 'FLIGHTS', 'CAR_RENTAL', 'OTHER'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    attachment = db.Column(db.Binary, )
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
 
 
 class ExpenseForm(Form):
@@ -45,13 +50,63 @@ class ExpenseForm(Form):
     submit = SubmitField('Submit')
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    expenses = db.relationship('Expense')
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Text, nullable=False)
+    currency = db.Column(db.Enum('GBR', 'EUR'), nullable=False)
+    concept = db.Column(db.Enum('MEALS', 'ACCOMODATION', 'TRANSPORT', 'FLIGHTS', 'CAR_RENTAL', 'OTHER'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    attachment = db.Column(db.Text)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+def hash_password(password):
+    return generate_password_hash(password)
+
+
+def verify_password(password, password_hash):
+    return check_password_hash(password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route('/')
+@login_required
 def index():
-    expenses = Expense.query.all()
-    return render_template('index.html', expenses=expenses)
+    return render_template('index.html', expenses=current_user.expenses)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and verify_password(form.password.data, user.password):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('index'))
+        flash('Invalid username or password.')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add():
     form = ExpenseForm(date=date.today())
     if form.validate_on_submit():
@@ -60,13 +115,13 @@ def add():
             filename = str(uuid.uuid4())
             urllib.urlretrieve(form.image.data, os.path.join(app.config['EXPENSES_UPLOAD'], filename))
 
-        expense = Expense(
+        db.session.add(Expense(
+            user=current_user.id,
             price=str(form.price.data.quantize(TWOPLACES)),
             currency=form.currency.data,
             concept=form.concept.data,
             date=form.date.data,
-            attachment=filename)
-        db.session.add(expense)
+            attachment=filename))
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
@@ -74,7 +129,6 @@ def add():
 
 @app.route('/uploads/<image>')
 def uploads(image):
-    print 'llego'
     return send_from_directory(app.config['EXPENSES_UPLOAD'], image)
 
 
